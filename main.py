4b37062ce8139e637d4e6f56e35950e06a4c2af4
mohamedraslan85 <75303084+mohamedraslan85@users.noreply.github.com>
2021-05-24 02:41:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 02:40:36 2021
+
+@author: mraslan
+"""
+
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
